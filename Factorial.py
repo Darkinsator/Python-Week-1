3f9a2f33ffbe2d70d83fa60171ff15042b9ec11f
Darkinsator <112648301+Darkinsator@users.noreply.github.com>
2024-03-12 11:33:54 +0000

    factorial

--- Factorial.py
+++ Factorial.py
@@ -1,24 +1,27 @@
-# Python program to find the factorial of a number provided by the user
-# using recursion
 
 def factorial(x):
-    """This is a recursive function
-    to find the factorial of an integer"""
 
     if x == 1:
         return 1
     else:
-        # recursive call to the function
+        
         return (x * factorial(x-1))
 
-
-
-
-#To take input from the user
 num = int(input("Enter a number: "))
 
-# call the factorial function
 result = factorial(num)
 print("The factorial of", num, "is", result)
+
+
+
+def calculation():
+    number1 = int(input("num 1"))
+    number2 = int(input("num 2"))
+    return (number1 + number2)
+    
+
+total = calculation()
+
+print(total)
 
 
