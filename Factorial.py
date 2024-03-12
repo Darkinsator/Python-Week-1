f48eafc90aa02918aede9183836ac0070d85fa98
Darkinsator <112648301+Darkinsator@users.noreply.github.com>
2024-03-12 07:46:02 +0000

    added factorial program

---
+++ Factorial.py
@@ -0,0 +1,14 @@
+
+name = ""
+number = ""
+
+print("Welcome to the factorial program.")
+
+while name == "":
+    input(str("Please enter your name"))
+
+while number == "":
+    input(str("Please enter a number to get the factorial value"))
+    
+for i in number:
+    print(number) 
--- HelloWorld.py
+++ HelloWorld.py
@@ -6,3 +6,9 @@ if OmegaString == "Hello World":
     print(BetaString + " it was fun.")
 
 
+name = ""
+
+while name == "":
+    name = input(str("Input your name."))
+
+print("You finally entered you name "+name)
